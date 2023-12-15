2985e6881bd20e7435421f32eafad63cbcc04e8a
Feliciano Franco <ffrancobme@gmail.com>
2023-12-15 12:57:14 -0300

    sliders y fórmula de colebrook

--- script.js
+++ script.js
@@ -2,11 +2,104 @@ document.addEventListener("DOMContentLoaded", function () {
   const canvas = document.getElementById("moodyCanvas");
   const ctx = canvas.getContext("2d");
   const moodyImage = document.getElementById("moodyImage");
-  const reynoldsElem = document.getElementById("reynolds");
-  const fdarcyElem = document.getElementById("fdarcy");
-  const indicador = document.getElementById("indicador");
-  let linesActive = true;
   const viewport = window.visualViewport;
+
+  const sliderRe = document.getElementById("reslider");
+  const outputRe = document.getElementById("redemo");
+  const sliderRo = document.getElementById("roughnessslider");
+  const outputRo = document.getElementById("roughnessdemo");
+  const outputF = document.getElementById("fdarcydemo");
+
+  let linesActive = true;
+  let reVar, eVar;
+
+  sliderRe.addEventListener("input", updateValues);
+  sliderRo.addEventListener("input", updateValues);
+
+  outputRe.addEventListener("input", updateValues2);
+  outputRo.addEventListener("input", updateValues2);
+
+  moodyImage.addEventListener("load", function () {
+    setCanvas();
+    updateValues();
+  });
+
+  function updateValues2() {
+    reVar = outputRe.value;
+    eVar = outputRo.value;
+    var sliderReValue = calculateSliderValue(reVar)
+    var sliderEValue = 100000 * eVar
+
+    sliderRe.setAttribute('value', sliderReValue);
+    sliderRe.value = sliderReValue;
+    sliderRo.setAttribute('value', sliderEValue);
+    sliderRo.value = sliderEValue;
+    const fVar = moody(reVar, eVar, 10);
+    outputF.innerHTML = fVar.toFixed(10);
+
+    const { x, y } = calcularCordenadas(reVar, fVar);
+    drawReferenceLine(x, y);
+    drawFactorCurve(reVar, eVar);
+  }
+
+  viewport.addEventListener("resize", setCanvas);
+
+  const timestamp = new Date().getTime();
+  moodyImage.src = "moody.png?t=" + timestamp;
+
+  function calculateReynolds(sliderValue) {
+    return Math.trunc(Math.pow(10.0, sliderValue / 10000.0));
+  }
+
+  function calculateSliderValue(re) {
+    return Math.log10(re) * 10000;
+  }
+
+  function updateValues() {
+    reVar = calculateReynolds(sliderRe.value);
+    eVar = sliderRo.value / 100000.0;
+
+    outputRe.setAttribute('value', reVar);
+    outputRe.value = reVar;
+    outputRo.setAttribute('value', eVar);
+    outputRo.value = eVar;
+    const fVar = moody(reVar, eVar, 10);
+    outputF.innerHTML = fVar.toFixed(15);
+
+    const { x, y } = calcularCordenadas(reVar, fVar);
+    drawReferenceLine(x, y);
+    drawFactorCurve(reVar, eVar);
+  }
+
+  function moody(Re, rel_e, n) {
+    if (Re <= 2300) {
+      return 64 / Re;
+    }
+
+    if (Re < 4000) {
+      return "transition flow";
+    }
+
+    let x = 0;
+    let y = 0;
+    let y_prime = 0;
+    let A = rel_e / 3.7;
+    let B = 2.51 / Re;
+
+    if (n === undefined) {
+      n = 3;
+    }
+
+    x = -1.8 * Math.log10((6.9 / Re) + Math.pow(A, 1.11));
+
+    for (let i = 1; i <= n; i++) {
+      y = x + 2 * Math.log10(A + B * x);
+      y_prime = 1 + 2 * (B / Math.log(10)) / (A + B * x);
+      x = x - y / y_prime;
+    }
+
+    return 1 / (x * x);
+  }
 
   function checkPosition(mouseX, mouseY) {
     const minX = 0.07 * canvas.width;
@@ -18,21 +111,18 @@ document.addEventListener("DOMContentLoaded", function () {
     return { res, minX, minY, maxX, maxY };
   }
 
-  // Función para dibujar las líneas de referencia
   function drawReferenceLine(x, y) {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
 
     const { res, minX, minY, maxX, maxY } = checkPosition(x, y);
 
-
-
-    let x1 = x + (maxY - minY)*0.02
-    if (x1 > maxX){
+    let x1 = x + (maxY - minY) * 0.02
+    if (x1 > maxX) {
       x1 = maxX;
     }
 
-    let y1 = y - (maxY - minY)*0.02
-    if (y1 < minY){
+    let y1 = y - (maxY - minY) * 0.02
+    if (y1 < minY) {
       y1 = minY;
     }
 
@@ -47,13 +137,44 @@ document.addEventListener("DOMContentLoaded", function () {
       ctx.beginPath();
       ctx.moveTo(minX, y);
       ctx.lineTo(x1, y);
-      ctx.strokeStyle = "red";
+      ctx.strokeStyle = "blue";
       ctx.lineWidth = 2;
       ctx.stroke();
     }
   }
 
-  function calcularReynolds(mouseX, mouseY, canvas) {
+  function drawFactorCurve(re, epsilon) {
+    ctx.beginPath();
+
+    if (re <= 2300) {
+      var { x, y } = calcularCordenadas(640, moody(640, 0))
+      ctx.moveTo(x, y);
+      var { x, y } = calcularCordenadas(2300, moody(2300, 0))
+      ctx.lineTo(x, y);
+    } else {
+      let reMinEscalado = 36021;
+      let reMin = Math.pow(10.0, reMinEscalado / 10000.0)
+      let fDarcy = moody(reMin, epsilon, 10)
+      var { x, y } = calcularCordenadas(reMin, fDarcy)
+      ctx.moveTo(x, y);
+
+      for (let reEscalado = reMinEscalado; reEscalado <= 80661; reEscalado += 661) {
+        let reTest = Math.pow(10.0, reEscalado / 10000.0);
+        fDarcy = moody(reTest, epsilon, 10);
+        if (fDarcy < 0.008) {
+          break;
+        }
+        var { x, y } = calcularCordenadas(reTest, fDarcy);
+        ctx.lineTo(x, y);
+      }
+    }
+
+    ctx.strokeStyle = "red";
+    ctx.lineWidth = 2;
+    ctx.stroke();
+  }
+
+  function calcularCordenadas(Re, f) {
     const x1 = 0.10962128810043101;
     const x2 = 0.8771750255885363;
     const y1 = 0.09;
@@ -68,57 +189,16 @@ document.addEventListener("DOMContentLoaded", function () {
     const D = (y3 - y4) / (x3 - x4);
     const C = Math.pow(10, (x4 * y3 - x3 * y4) / (x4 - x3));
 
-    const reynolds = Math.trunc(C * Math.pow(10, D * mouseX / canvas.width));
-    const factorFriccion = (A * Math.pow(10, B * mouseY / canvas.height)).toFixed(6);
+    const xCoord = canvas.width / D * (Math.log10(Re) - Math.log10(C))
+    const yCoord = canvas.height / B * (Math.log10(f) - Math.log10(A))
 
-    return { reynolds, factorFriccion };
+    return { x: xCoord, y: yCoord };
   }
 
-  function handleMouseMove(event) {
-    if (linesActive) {
-      const rect = canvas.getBoundingClientRect();
-      const mouseX = event.clientX - rect.left;
-      const mouseY = event.clientY - rect.top;
-
-      const { res, minX, minY, maxX, maxY } = checkPosition(mouseX, mouseY);
-
-      if (res) {
-
-        drawReferenceLine(mouseX, mouseY);
-
-        const { reynolds, factorFriccion } = calcularReynolds(mouseX, mouseY, canvas);
-
-        console.log(`(${reynolds}, ${factorFriccion})`);
-
-        reynoldsElem.textContent = `Re = ${reynolds}`;
-        fdarcyElem.textContent = `f = ${factorFriccion}`;
-      }
-    }
-  }
-
-
-  // Función para cambiar el estado de las líneas
-  function toggleLines() {
-    linesActive = !linesActive;
-  }
-
-  // Función para ajustar el tamaño del canvas
   function setCanvas() {
     canvas.width = moodyImage.width;
     canvas.height = moodyImage.height;
-    indicador.style.top = `${canvas.height} px`;
-    indicador.style.left = `${0.1 * canvas.width} px`;
-    console.log("test");
-    console.log(indicador.style);
+    //indicador.style.top = `${canvas.height}px`;
+    //indicador.style.left = `${0.1 * canvas.width}px`;
   }
-
-  // Eventos
-  moodyImage.addEventListener("load", setCanvas);
-  viewport.addEventListener("resize", setCanvas);
-
-  canvas.addEventListener("mousemove", handleMouseMove);
-  canvas.addEventListener("click", toggleLines);
-
-  const timestamp = new Date().getTime();
-  moodyImage.src = "moody.png?t=" + timestamp;
 });
